29173d136bca9279a3146eaedd804020dfc14bf4
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-05-06 17:49:11 +0200

    nique le CSS - done

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,15 +1,25 @@
-import TravelPlan from "../src/assets/travel-plans.json";
-import { useState } from "react";
+import { useState } from 'react';
+import travelPlansData from '../assets/travel-plans.json';
 
-function TravelList() {
-  const [TravelIdeas, setTravelIdeas] = useState(TravelPlan);
+const TravelList = () => {
+  const [plans, setPlans] = useState(travelPlansData);
+
+  const handleDelete = (id) => {
+    setPlans(plans.filter(plan => plan.id !== id));
+  };
 
   return (
-    <div>
-      <h2>Travel List</h2>
-      {TravelPlan.map((TravelIdeas) => {
-        return qsd;
-      })}
+    <div className='travel-card'>
+      {plans.map(plan => (
+        <li key={plan.id}>
+          <img src={plan.image} alt="" />
+          {plan.destination}
+          {plan.totalCost <= 350 && <span> - Great Deal</span>}
+          {plan.totalCost >= 1500 && <span> - Premium</span>}
+          {plan.allInclusive && <span> - All Inclusive</span>}
+          <button onClick={() => handleDelete(plan.id)}>Delete</button>
+        </li>
+      ))}
     </div>
   );
 }
